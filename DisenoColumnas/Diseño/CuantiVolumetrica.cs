ac5a0968659ada2100cf0c875f96cf42c6975c9a
svasquez <svasquez@controldiseno.com>
2019-11-13 10:44:36 -0500

    Correccionen entrada de usuario

--- DisenoColumnas/Diseño/CuantiVolumetrica.cs
+++ DisenoColumnas/Diseño/CuantiVolumetrica.cs
@@ -1,9 +1,9 @@
 ﻿using DisenoColumnas.Clases;
 using DisenoColumnas.Secciones;
-using SpannedDataGridViewNet2;
 using System;
 using System.Drawing;
 using System.Windows.Forms;
+using WeifenLuo;
 using WeifenLuo.WinFormsUI.Docking;
 
 namespace DisenoColumnas.Diseño
